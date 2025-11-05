2ba4e7cc1d4da622e3ebd5996f3f8777a6e10c6a
chabeerabsal <chabeerabsal@gmail.com>
2025-11-05 22:57:52 +0530

    Added Controller

--- CloudBasedOnlineAccountingSoftware/src/main/java/com/example/CloudBasedOnlineAccountingSoftware/Controller/UserController.java
+++ CloudBasedOnlineAccountingSoftware/src/main/java/com/example/CloudBasedOnlineAccountingSoftware/Controller/UserController.java
@@ -1,4 +1,14 @@
 package com.example.CloudBasedOnlineAccountingSoftware.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounting")
 public class UserController {
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
 }
