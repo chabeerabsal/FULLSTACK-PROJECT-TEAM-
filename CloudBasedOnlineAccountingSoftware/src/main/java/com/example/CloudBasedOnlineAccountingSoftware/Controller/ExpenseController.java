80cf1f47e458d2fc8ffd0252219408f07a46dd10
chabeerabsal <chabeerabsal@gmail.com>
2025-11-07 22:58:48 +0530

    Added Controller for Expenses

--- CloudBasedOnlineAccountingSoftware/src/main/java/com/example/CloudBasedOnlineAccountingSoftware/Controller/ExpenseController.java
+++ CloudBasedOnlineAccountingSoftware/src/main/java/com/example/CloudBasedOnlineAccountingSoftware/Controller/ExpenseController.java
@@ -1,21 +1,24 @@
 package com.example.CloudBasedOnlineAccountingSoftware.Controller;
 
-
-
-
 import com.example.CloudBasedOnlineAccountingSoftware.Model.Expense;
 import com.example.CloudBasedOnlineAccountingSoftware.Service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/expenses")
-@CrossOrigin(origins = "http://localhost:4200")
 public class ExpenseController {
 
     @Autowired
     private ExpenseService expenseService;
+
+    @PostMapping
+    public Expense createExpense(@RequestBody Expense expense) {
+        return expenseService.createExpense(expense);
+    }
 
     @GetMapping
     public List<Expense> getAllExpenses() {
@@ -23,13 +26,8 @@ public class ExpenseController {
     }
 
     @GetMapping("/{id}")
-    public Expense getExpenseById(@PathVariable Long id) {
+    public Optional<Expense> getExpenseById(@PathVariable Long id) {
         return expenseService.getExpenseById(id);
-    }
-
-    @PostMapping
-    public Expense createExpense(@RequestBody Expense expense) {
-        return expenseService.createExpense(expense);
     }
 
     @PutMapping("/{id}")
@@ -38,8 +36,8 @@ public class ExpenseController {
     }
 
     @DeleteMapping("/{id}")
-    public void deleteExpense(@PathVariable Long id) {
+    public String deleteExpense(@PathVariable Long id) {
         expenseService.deleteExpense(id);
+        return "Expense deleted successfully.";
     }
 }
-
